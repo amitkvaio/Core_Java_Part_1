7922c711c346423a1567c9011d19cc73d7426c86
amitkvaio <46754383+amitkvaio@users.noreply.github.com>
2021-12-02 19:52:28 +0530

    Adding example for static import
    Adding example for static import

--- 53 Static import/src/pack1/A.java
+++ 53 Static import/src/pack1/A.java
@@ -5,7 +5,7 @@ public class A
 	public static int i = 20;
 	public static void test()
 	{
-		System.out.println("done");
+		System.out.println("test method from A class");
 	}
 }
 /*
@@ -13,4 +13,16 @@ class A is public we can use outside pack1 also.
 inside class A we are keeping the two member that is public so
 we can use outside pack1 also
 
+
+Java Static Import
+
+The static import feature of Java 5 facilitate the java programmer to access 
+	any static member of a class directly. There is no need to qualify it by the class name.
+	
+Advantage of static import:
+   Less coding is required if you have access any static member of a class oftenly.
+
+Disadvantage of static import:
+    If you overuse the static import feature, 
+    it makes the program unreadable and unmaintainable
 */
